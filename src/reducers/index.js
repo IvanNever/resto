d2e890acb0cc1f27d6d9da990f1933f1b25008dd
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-08-06 17:25:00 +0300

    refactor item cards in cart, count total price in header and post order to json

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
+// параметр по умолчанию
 const initialState = {
     menu: [],
     loading: true,
     error: false,
-    items: []
+    items: [],
+    totalPrice: 0
 }
+
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
@@ -23,35 +26,61 @@ const reducer = (state = initialState, action) => {
             };
         case 'MENU_ERROR':
             return {
+                ...state,
                 menu: state.menu,
-                loading: true,
                 error: true
             };
         case 'ITEM_ADD_TO_CART':
             const id = action.payload;
-            const item = state.menu.find(item => item.id === id)
+
+            const itemInd = state.items.findIndex(item => item.id === id);
+            if (itemInd >= 0){
+                const itemInState = state.items.find(item => item.id === id);
+                const newItem = {
+                    ...itemInState,
+                    qtty: ++itemInState.qtty
+                }
+                return {
+                    ...state,
+                    items: [
+                        ...state.items.slice(0, itemInd),
+                        newItem,
+                        ...state.items.slice(itemInd + 1)
+                    ],
+                    totalPrice: state.totalPrice + newItem.price
+                }
+
+            }
+            // товара раньше не было в корзине
+            const item = state.menu.find(item => item.id === id);
             const newItem = {
                 title: item.title,
-                url: item.url,
                 price: item.price,
-                id: item.id
-            }
+                url: item.url,
+                id: item.id,
+                qtty: 1
+            };
+
             return {
                 ...state,
                 items: [
                     ...state.items,
                     newItem
-                ]
+                ],
+                totalPrice: state.totalPrice + newItem.price
             };
+
         case 'ITEM_REMOVE_FROM_CART':
             const idx = action.payload;
-            const itemIndex = state.items.findIndex(item => item.id === idx);
+            const itemIndex = state.items.findIndex(item => item.id === idx)
+            const price = state.items[itemIndex]['price'] * state.items[itemIndex]['qtty'];
             return {
                 ...state,
                 items: [
                     ...state.items.slice(0, itemIndex),
                     ...state.items.slice(itemIndex + 1)
-                ]
+                ],
+                totalPrice: state.totalPrice - price
             }
         default:
             return state;
